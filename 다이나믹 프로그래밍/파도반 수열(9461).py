83bdc8cd5b4b420077d69cb8205ac9f50d34e256
lmj101910 <71292878+lmj101910@users.noreply.github.com>
2022-01-05 10:48:20 +0900

    Create 파도반 수열(9461).py

---
+++ 다이나믹 프로그래밍/파도반 수열(9461).py
@@ -0,0 +1,12 @@
+n = int(input())
+
+dp = [0] * 101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+for i in range(4,101):
+    dp[i] = dp[i-3] + dp[i-2]
+
+for i in range(n):
+    k = int(input())
+    print(dp[k])
